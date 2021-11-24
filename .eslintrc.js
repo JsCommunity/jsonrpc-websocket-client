64b106cfc0a7fbf4b9ad3c6fa7126aeb76c979f0
Julien Fontanet <julien.fontanet@isonoe.net>
2021-11-24 23:01:13 +0100

    chore: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,6 @@ module.exports = {
 
     // disable rules handled by prettier
     "prettier",
-    "prettier/standard",
   ],
 
   parserOptions: {
@@ -18,18 +17,6 @@ module.exports = {
   },
 
   rules: {
-    // prefer let/const over var
-    "no-var": "error",
-
-    // prefer const over let when possible
-    //
-    // should be included in standard: https://github.com/standard/eslint-config-standard/pull/133/
-    "prefer-const": "error",
-
-    // detect incorrect import
-    "node/no-extraneous-import": "error",
-    "node/no-missing-import": "error",
-
     // uncomment if you are using a builder like Babel
     "node/no-unsupported-features/es-syntax": "off",
   },
